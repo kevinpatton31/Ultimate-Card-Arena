57d797ef4f4b11cdbd242c6efaad26d6b39dbfae
Fábio <Fabiojscosta1583@gmail.com>
2014-10-30 22:28:48 +0000

    Start

---
+++ UltimateCardBattle/src/towlogic/solutions/ultimatecardbattle/MainMenu_1.java
@@ -0,0 +1,15 @@
+package towlogic.solutions.ultimatecardbattle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainMenu_1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu_1);
+	}
+}
