01433f9f5ad5296882c4d6aedffde6fe9fcab01b
Fábio <Fabiojscosta1583@gmail.com>
2014-10-30 18:36:56 +0000

    Inicio

---
+++ UltimateCardBattle/gen/towlogic/solutions/ultimatecardbattle/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package towlogic.solutions.ultimatecardbattle;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
